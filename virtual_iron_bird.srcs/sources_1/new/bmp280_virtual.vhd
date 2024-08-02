def7fe5f2ea3e8e18be0caf9f77142c20c94bf62
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2024-08-02 18:59:04 +0200

    refactor i2c slave bmp

--- virtual_iron_bird.srcs/sources_1/new/bmp280_virtual.vhd
+++ virtual_iron_bird.srcs/sources_1/new/bmp280_virtual.vhd
@@ -21,15 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity bmp280_virtual is
     Generic (
@@ -49,13 +41,21 @@ architecture Behavioral of bmp280_virtual is
     signal s_is_starting : std_logic := '0';
     signal s_is_ending   : std_logic := '0';
     
-    signal s_rst : std_logic := '0';
+    signal s_rst   : std_logic := '0';
+    signal s_is_me : std_logic := '0';
     
-    signal s_state: State_type := Ended;
-    signal s_control_state: Control_State_type := Read_Address;
-    signal s_command: Command_type := Read_From_Bus;
-    signal s_command_addr: std_logic_vector(7 downto 0) := (others => '0');
-    signal s_has_read_command_addr: std_logic := '0';
+    signal s_bit_counter      : integer range 0 to 7 := 0;
+    signal s_bit_counter_next : integer range 0 to 8 := 0;
+    signal current_read_byte  : std_logic_vector(7 downto 0) := (others => '0');
+    signal current_write_byte : std_logic_vector(7 downto 0) := (others => '0');
+
+    signal s_state                 : State_type := Ended;
+    signal s_is_ack                : std_logic := '0';
+    signal s_is_last_ack_valid     : std_logic := '0';
+    signal s_is_reading_addr       : std_logic := '1';
+    signal s_is_writing_to_bus     : std_logic := '0';
+    signal s_command_addr          : std_logic_vector(7 downto 0) := (others => '0');
+    signal s_has_read_command_addr : std_logic := '0';
     signal s_command_addr_first_set: std_logic := '1';
 begin
     -------------------------------------------
@@ -85,118 +85,130 @@ begin
     s_state <= Starting when s_is_starting = '1' else
                Ending   when s_is_ending = '1' else
                Started  when s_state = Starting else
-               Ended    when s_state = Ending;
+               Ended    when s_state = Ending else s_state;
     s_rst <= '1' when s_state = Starting or s_state = Ending else '0';
     
-    -- Packet receiver
+    -- Bit counter logic
     process (i_SCL, s_rst) is
-        variable current_msg    : std_logic_vector(7 downto 0) := (others => '0');
-        variable bit_counter    : integer := 0;
-        variable previous_state : Control_State_type := Read_Address;
-        variable skip_ack       : std_logic := '0';
-        variable end_next_cmd   : std_logic := '0';
-        variable current_sda    : std_logic := '0';
     begin
         if s_rst = '1' then
-            -- Reset states
-            if s_state = Starting then
-                bit_counter := 0;
-                previous_state := Read_Address;
-                end_next_cmd := '0';
-                s_command <= Read_From_Bus;
-                s_control_state <= Read_Address;
-            elsif s_state = Ending then
-                s_has_read_command_addr <= '0';
-                s_command_addr <= (others => '0');
-                s_command_addr_first_set <= '1';
-            end if;
-        else
-            skip_ack := '0';
-            if rising_edge(i_SCL) and s_state = Started and end_next_cmd = '0' then
-                if s_control_state /= Ack then
-                    if s_command = Read_From_Bus or s_control_state = Read_Address then
-                        current_msg(7 - bit_counter) := io_SDA;
-                    end if;
-                    bit_counter := bit_counter + 1;
-                    previous_state := s_control_state;
-                    if bit_counter = 8 then
-                        s_control_state <= Ack;
-                        if s_command = Read_From_Bus and s_control_state = Execute_command and s_has_read_command_addr = '0' then
-                            s_has_read_command_addr <= '1';
-                            s_command_addr <= current_msg;
-                        end if;
-                    end if;
-                elsif s_command = Write_To_Bus then
-                    assert io_SDA = '0'
-                        report "SDA Master ACK should be low"
-                        severity ERROR;
-                    if io_SDA /= '0' then
-                        end_next_cmd := '1';
-                    end if;
-                end if;
-            end if;
-            if falling_edge(i_SCL) and s_state = Started and end_next_cmd = '0' then
-                if s_control_state = Ack then
-                    if bit_counter = 8 then
-                        bit_counter := 0;
-                        if previous_state = Read_Address then
-                            case current_msg(0) is
-                                when '0' =>
-                                    s_command <= Read_From_Bus;
-                                when '1' =>
-                                    s_command <= Write_To_Bus;
-                                when others =>
-                                    s_command <= Read_From_Bus;
-                            end case;
-                        end if;
-                    else
-                        skip_ack := '1';
-                        case previous_state is
-                            when Read_Address =>
-                                s_control_state <= Execute_command;                          
-                            when Execute_command =>
-                                if s_command_addr_first_set = '1' then
-                                    s_command_addr <= s_command_addr;
-                                    s_command_addr_first_set <= '0';
-                                else
-                                    s_command_addr <= std_logic_vector(to_unsigned(to_integer(unsigned(s_command_addr)) + 1, 8));
-                                end if;
-                                s_control_state <= Execute_command;
-                            when others =>
-                                s_control_state <= previous_state;
-                        end case;
-                        if s_command = Write_To_Bus then
-                            -------------------------------------------------
-                            -- OUTCOMING REGISTER
-                            -------------------------------------------------
-                            case s_command_addr is
-                                when x"D0" => -- ID Register
-                                    current_msg := x"58";
-                                when others => current_msg := (others => '0');
-                            end case;
-                            current_sda := current_msg(7 - bit_counter);
-                        end if;
-                    end if;
-                elsif s_command = Write_To_Bus then
-                    current_sda := current_msg(7 - bit_counter);
-                end if;
-            end if;
-        end if;
-        
-        -- Set Ack
-        if s_command = Read_From_Bus then
-            if s_control_state = Ack and skip_ack = '0' then
-                io_SDA <= '0';
-            else
-                io_SDA <= 'Z';
-            end if;
-        elsif s_command = Write_To_Bus then
-            if s_control_state = Ack and skip_ack = '0' and previous_state /= Read_Address then
-                io_SDA <= 'Z';
-            else
-                io_SDA <= current_sda;
+            s_bit_counter <= 0;
+        elsif falling_edge(i_SCL) then
+            if s_is_ack = '0' then
+                s_bit_counter <= s_bit_counter_next;
             end if;
         end if;
     end process;
+    s_bit_counter_next <= s_bit_counter + 1 when s_bit_counter /= 7 and s_state = Started else 0;
+    
+    -- Acknowledge State
+    process (i_SCL, s_rst) is
+    begin
+        if s_rst = '1' then
+            s_is_ack <= '0';
+        elsif falling_edge(i_SCL) then
+            if s_bit_counter = 7 then
+                s_is_ack <= '1';
+            else
+                s_is_ack <= '0';
+            end if;
+        end if;
+    end process;
+    -- Acknowledge Verify
+    process (i_SCL, s_rst) is
+    begin
+        if s_rst = '1' then
+            s_is_last_ack_valid <= '1';
+        elsif rising_edge(i_SCL) then
+            s_is_last_ack_valid <= s_is_last_ack_valid;
+            if s_is_ack = '1' then
+                if io_SDA = '0' then
+                    s_is_last_ack_valid <= '1';
+                else
+                    s_is_last_ack_valid <= '0';
+                end if;
+            end if;
+        end if;
+    end process;
+ 
+    -- First address reading
+    process (s_is_ack, s_is_starting) is
+    begin
+        if s_is_starting = '1' then
+            s_is_reading_addr <= '1';
+        elsif falling_edge(s_is_ack) then
+            s_is_reading_addr <= '0';
+        end if;
+    end process;
+    
+    -- Address check
+    process (s_is_ack, s_is_starting) is
+    begin
+        if s_is_starting = '1' then
+            s_is_me <= '1';
+        elsif rising_edge(s_is_ack) then
+            if s_is_reading_addr = '1' then
+                if current_read_byte(7 downto 1) = std_logic_vector(I2C_ADRR) then
+                    s_is_me <= '1';
+                else
+                    s_is_me <= '0';
+                end if;
+            end if;
+        end if;
+    end process;
+    
+    -- Read R/W flag
+    process (s_is_ack, s_is_starting) is
+    begin
+        if s_is_starting = '1' then
+            s_is_writing_to_bus <= '0';
+        elsif rising_edge(s_is_ack) and s_is_reading_addr = '1' then
+            s_is_writing_to_bus <= current_read_byte(0);
+        end if;
+    end process;
 
+    -------------------------------------------
+    -- READ FROM BUS LOGIC
+    -------------------------------------------
+    process (i_SCL, s_rst) is
+    begin
+        if s_rst = '1' then
+            current_read_byte <= (others => '0');
+        elsif rising_edge(i_SCL) and s_is_ack = '0'  then
+            current_read_byte(7 - s_bit_counter) <= io_SDA;
+        end if;
+    end process;
+    
+    -------------------------------------------
+    -- WRITE TO BUS LOGIC
+    -------------------------------------------
+    current_write_byte <= x"58" when s_command_addr = x"D0" else (others => '0');
+    
+    -------------------------------------------
+    -- HIGH LEVEL LOGIC LOGIC
+    -------------------------------------------
+    process (s_is_ack, s_is_ending) is
+    begin
+        if s_is_ending = '1' then
+            s_has_read_command_addr <= '0';
+            s_command_addr <= (others => '0');
+        elsif rising_edge(s_is_ack) then
+            s_has_read_command_addr <= s_has_read_command_addr;
+            s_command_addr <= s_command_addr;
+            if s_is_reading_addr = '0' then
+                if s_has_read_command_addr = '0' then
+                    s_has_read_command_addr <= '1';
+                    s_command_addr <= current_read_byte;
+                else
+                    s_command_addr <= std_logic_vector(unsigned(s_command_addr) + 1);
+                end if;
+            end if;
+        end if;
+    end process;
+    
+    io_SDA <= 'Z' when s_is_last_ack_valid = '0' or s_is_ending = '1' or s_is_me = '0' else
+              '0' when s_is_ack = '1' and s_is_writing_to_bus = '0' else
+              '0' when s_is_ack = '1' and s_is_reading_addr = '1' else
+              current_write_byte(7 - s_bit_counter) when s_is_writing_to_bus = '1' and s_is_ack = '0' else
+              'Z';
 end Behavioral;
